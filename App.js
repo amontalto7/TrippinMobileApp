a18de40fca017427625e46bc3b71f766b7ef8a6d
Clos Devop <31907938+cgarcia101015@users.noreply.github.com>
2019-03-18 20:32:21 -0400

    mobile

--- App.js
+++ App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
-
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
-
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
@@ -23,37 +21,33 @@ export default class App extends React.Component {
       }),
     ]);
   };
-
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
     console.warn(error);
   };
-
   _handleFinishLoading = () => {
     this.setState({ isLoadingComplete: true });
   };
-}
-
-render() {
-  if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={this._loadResourcesAsync}
-        onError={this._handleLoadingError}
-        onFinish={this._handleFinishLoading}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
+    }
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ screens/location-recognition.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const translate = require("./PhrasesScreen")
+const recogLang = require("./language_recognition");
+var URL = "https://www.ipapi.co/";
+// console.log(URL);
+URL += "json";
+
+axios.get(URL).then(async response => {
+    // const geoLoc = await recogLang(response.data.ip);
+
+    const geoLoc = await recogLang("85.214.132.117");
+    console.log(geoLoc);
+    translate(geoLoc.countryCode);
+
+});
