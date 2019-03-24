5e97659f0ea31b94c8bbfcc9e610394e9e5c8bd2
ant4u <anthonyimontalto@gmail.com>
2019-03-24 01:47:49 -0400

    styling

--- components/Location.js
+++ components/Location.js
@@ -64,7 +64,7 @@ export default class Location extends Component {
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={this.findCoordinates}>
-          <Text>{this.state.message}</Text>
+          <Text style={styles.welcome}>{this.state.message}</Text>
         </TouchableOpacity>
       </View>
     );
